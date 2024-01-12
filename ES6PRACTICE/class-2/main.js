6395eb422b46681b3c796dde339cd60875a9f411
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-12 22:44:33 +0600

    node js

--- ES6PRACTICE/class-2/main.js
+++ ES6PRACTICE/class-2/main.js
@@ -14,13 +14,18 @@ laptop.freeStorage(512,170);
 
 
 class laptop{
+
     brand="Apple"
-    ram=8
+    ram=16
     isM1=true
     display=16.2
 
     freeStoragename(total,used)=>{
         console.log(total-used);
-
     }
 }
+
+let LaptoObj= new laptop();
+
+LaptoObj.freeStoragename(300-200);
+console.log(LaptoObj.brand);
