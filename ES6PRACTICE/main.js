f7eb7855d526164b8fd62bd0e660e59102138bcd
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-12 17:20:24 +0600

    node js

--- ES6PRACTICE/main.js
+++ ES6PRACTICE/main.js
@@ -1,21 +1,16 @@
 // JavaScript Strict Mode
+
+
 /*"use strict"
 var x=3.14
 console.log(x);*/
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 //let and const for Variable Declaration
+
+let a=10;
+a=20;
+console.log(a);
+
+
